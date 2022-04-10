365d6dbdfdf2af8d978e6bafcb05a8fbc1048daf
Shalini7601 <96128917+Shalini7601@users.noreply.github.com>
2022-04-10 08:22:18 -0700

    Add files via upload

---
+++ pratical/operatorExample.java
@@ -0,0 +1,13 @@
+class operatorExample{
+public static void main(String args[]){
+int a=18, b=24;
+System.out.println("a+b=" +(a+b));
+System.out.println("a*b=" +(a*b));
+System.out.println(a == b);
+System.out.println(a > b);
+System.out.println(a <= b);
+System.out.println(a/b);
+System.out.println((18>24)&&(18<24));
+System.out.println((18>24)||(18<24));
+}
+}
